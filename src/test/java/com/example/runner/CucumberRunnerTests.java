33929895a9743a157577682c682f096d24c2bae0
Seedarla Dasu <Seedarla.Dasu@coforge.com>
2022-06-29 18:59:17 +0530

    Adding Cucumber reports

--- src/test/java/com/example/runner/CucumberRunnerTests.java
+++ src/test/java/com/example/runner/CucumberRunnerTests.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
  
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = "pretty", features = {"src/test/resources/Features"}, glue = { "com.example.definitions"})
+@CucumberOptions(plugin = {"json:target/cucumber/cucumber.json"}, features = {"src/test/resources/Features"}, glue = { "com.example.definitions"})
  
 public class CucumberRunnerTests {
 }
